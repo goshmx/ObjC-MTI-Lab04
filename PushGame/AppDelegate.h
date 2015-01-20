ff99ac543a172636e13820499411fedf95a1a2df
Gosh <osvaldocube@gmail.com>
2015-01-19 20:11:27 -0600

    Initial Commit

---
+++ PushGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushGame
+//
+//  Created by TRON on 19/01/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
